fdafff6f4776c8ce45200dd108cb910bb2b106c0
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-10-12 15:26:47 +0200

    Update bdd_python.py

--- bdd_python.py
+++ bdd_python.py
@@ -15,7 +15,7 @@ st.image('https://soliguide.fr/assets/images/logo.png',width=600)
 df = pd.read_csv("./data_csv/fiche_figure1.csv")
 s = pd.read_csv("./data_csv/searchWithDatePresentation2.csv")
 compteProCum = pd.read_csv("./data_csv/orga_figure3.csv")
-HtmlFile = open("ressources/france.html", 'r', encoding='utf-8')
+HtmlFile = open("./data_csv/map.html", 'r', encoding='utf-8')
 
 
 
