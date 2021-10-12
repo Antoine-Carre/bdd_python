2e49ec6d9143f664e96e651d45f4e29d89181869
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-10-12 17:39:46 +0200

    Update bdd_python.py

--- bdd_python.py
+++ bdd_python.py
@@ -13,6 +13,8 @@ st.set_page_config(page_title="Base de données - Septembre 2021",
 
 st.image('https://soliguide.fr/assets/images/logo.png',width=600)
 
+st.write("[Lien vers la documentation et les explications](https://www.notion.so/Suivi-des-indicateurs-mensuels-process-b285b4b9bb3b48f997f7cd8b728605d1)")
+
 df = pd.read_csv("./data_csv/fiche_figure1.csv")
 s = pd.read_csv("./data_csv/searchWithDatePresentation2.csv")
 compteProCum = pd.read_csv("./data_csv/orga_figure3.csv")
