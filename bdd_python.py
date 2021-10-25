a1c30f79d2a1f0bcce94cadeb50e00f50fcabe89
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-10-25 17:15:03 +0200

    Update bdd_python.py

--- bdd_python.py
+++ bdd_python.py
@@ -40,9 +40,9 @@ if TerrG=='général':
                                             'Mise à jour général cumulé',  'Fiches reliées aux comptes pros général cumulé'])
 
     s1 = s.filter(regex='général')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])
     
     figComptePro = px.bar(compteProCum, x='datePresentation', y='Général cumulé')
 
@@ -54,9 +54,9 @@ if TerrG=='06':
                                             'Mise à jour (dep:06) cumulé',  'Fiches reliées aux comptes pros (dep:06) cumulé'])
 
     s1 = s.filter(regex='06')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])
 
 if TerrG=='33':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:33)','En ligne (dep:33)', 'En brouillon (dep:33)','Mise à jour (dep:33)', 
@@ -65,9 +65,9 @@ if TerrG=='33':
                                             'Mise à jour (dep:33) cumulé',  'Fiches reliées aux comptes pros (dep:33) cumulé'])
 
     s1 = s.filter(regex='33')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])
 
 if TerrG=='44':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:44)','En ligne (dep:44)', 'En brouillon (dep:44)','Mise à jour (dep:44)', 
@@ -76,9 +76,9 @@ if TerrG=='44':
                                             'Mise à jour (dep:44) cumulé',  'Fiches reliées aux comptes pros (dep:44) cumulé'])    
 
     s1 = s.filter(regex='44')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='67':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:67)','En ligne (dep:67)', 'En brouillon (dep:67)','Mise à jour (dep:67)', 
@@ -87,9 +87,9 @@ if TerrG=='67':
                                             'Mise à jour (dep:67) cumulé',  'Fiches reliées aux comptes pros (dep:67) cumulé'])     
 
     s1 = s.filter(regex='67')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='75':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:75)','En ligne (dep:75)', 'En brouillon (dep:75)','Mise à jour (dep:75)', 
@@ -98,9 +98,9 @@ if TerrG=='75':
                                             'Mise à jour (dep:75) cumulé',  'Fiches reliées aux comptes pros (dep:75) cumulé'])      
 
     s1 = s.filter(regex='75')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                           
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                           
 
 if TerrG=='77':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:77)','En ligne (dep:77)', 'En brouillon (dep:77)','Mise à jour (dep:77)', 
@@ -109,9 +109,9 @@ if TerrG=='77':
                                             'Mise à jour (dep:77) cumulé',  'Fiches reliées aux comptes pros (dep:77) cumulé'])    
 
     s1 = s.filter(regex='77')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='78':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:78)','En ligne (dep:78)', 'En brouillon (dep:78)','Mise à jour (dep:78)', 
@@ -120,9 +120,9 @@ if TerrG=='78':
                                             'Mise à jour (dep:78) cumulé',  'Fiches reliées aux comptes pros (dep:78) cumulé'])    
 
     s1 = s.filter(regex='78')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='91':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:91)','En ligne (dep:91)', 'En brouillon (dep:91)','Mise à jour (dep:91)', 
@@ -131,9 +131,9 @@ if TerrG=='91':
                                             'Mise à jour (dep:91) cumulé',  'Fiches reliées aux comptes pros (dep:91) cumulé'])     
 
     s1 = s.filter(regex='91')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                           
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                           
 
 if TerrG=='92':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:92)','En ligne (dep:92)', 'En brouillon (dep:92)','Mise à jour (dep:92)', 
@@ -142,9 +142,9 @@ if TerrG=='92':
                                             'Mise à jour (dep:92) cumulé',  'Fiches reliées aux comptes pros (dep:92) cumulé'])   
 
     s1 = s.filter(regex='92')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='93':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:93)','En ligne (dep:93)', 'En brouillon (dep:93)','Mise à jour (dep:93)', 
@@ -153,9 +153,9 @@ if TerrG=='93':
                                             'Mise à jour (dep:93) cumulé',  'Fiches reliées aux comptes pros (dep:93) cumulé'])     
 
     s1 = s.filter(regex='93')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='94':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:94)','En ligne (dep:94)', 'En brouillon (dep:94)','Mise à jour (dep:94)', 
@@ -164,9 +164,9 @@ if TerrG=='94':
                                             'Mise à jour (dep:94) cumulé',  'Fiches reliées aux comptes pros (dep:94) cumulé'])     
 
     s1 = s.filter(regex='94')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                            
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                            
 
 if TerrG=='95':
     fig1 = px.line(df, x="Date_saisie", y=['Suivies (dep:95)','En ligne (dep:95)', 'En brouillon (dep:95)','Mise à jour (dep:95)', 
@@ -174,9 +174,9 @@ if TerrG=='95':
     figBar = px.bar(df, x="Date_saisie",y=['Suivies (dep:95) cumulé', 'En ligne (dep:95) cumulé', 'En brouillon (dep:95) cumulé', 
                                             'Mise à jour (dep:95) cumulé',  'Fiches reliées aux comptes pros (dep:95) cumulé'])      
     s1 = s.filter(regex='95')
-    s1 = pd.merge(s['Unnamed: 0'],s1, how='left', left_index=True, right_index=True)
+    s1 = pd.merge(s['datePresentation'],s1, how='left', left_index=True, right_index=True)
 
-    figSearch = px.line(s1,x='Unnamed: 0', y=s1.columns.values.tolist()[1:])                                               
+    figSearch = px.line(s1,x='datePresentation', y=s1.columns.values.tolist()[1:])                                               
 
 fig1.update_xaxes(title_text="Date de création (de la fiche ou du compte pro de la fiche) ou date de la dernière mise à jour de la fiche", title_standoff=0.6, title_font_family="Times New Roman")
 fig1.update_yaxes(title_text="Nombre de fiches (non cumulé)", title_font_family="Times New Roman")
