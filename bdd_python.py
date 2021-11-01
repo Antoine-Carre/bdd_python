f0a7e9216ce61743e8fbcc8767e4014d29168a55
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-11-01 20:25:54 +0100

    Update bdd_python.py

--- bdd_python.py
+++ bdd_python.py
@@ -423,7 +423,7 @@ if TerrG=='95':
 
     html_string_1 = f"""<br>
     <center><font face='Helvetica' size='6'>{df_maj_3_months.loc['Val-d\'Oise','pourcentage']} %</font>
-    <br/><font size='3'>des fiches ont été mise à jours au moins une fois pendant les 3 derniers mois<br></font></center>"""
+    <br><font size='3'>des fiches ont été mise à jours au moins une fois pendant les 3 derniers mois<br></font></center>"""
 
     html_string_2 = f"""<br>
     <center><font face='Helvetica' size='6'>{df_maj_6_months.loc['Val-d\'Oise','pourcentage']} %</font>
