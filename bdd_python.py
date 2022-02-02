e9ba60c773220012a1b548cb121659747d9f949c
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-02-02 23:03:14 +0100

    Update bdd_python.py

--- bdd_python.py
+++ bdd_python.py
@@ -18,6 +18,7 @@ st.write("[Lien vers les chiffres](https://airtable.com/appfuLygVTjBO0qk1/tblnFp
 
 
 df = pd.read_csv("./data_csv/fiche_figure1.csv")
+df = df.iloc[:-1]
 s = pd.read_csv("./data_csv/searchWithDatePresentation3.csv")
 compteProCum = pd.read_csv("./data_csv/orga_figure3.csv")
 df4 = pd.read_csv("data_csv/GAdata.csv")
