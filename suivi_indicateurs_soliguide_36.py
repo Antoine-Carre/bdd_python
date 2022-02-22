192e90527697774eac55ceca5cb52b48d6bafea3
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-02-22 14:32:38 +0100

    Update suivi_indicateurs_soliguide_36.py

--- suivi_indicateurs_soliguide_36.py
+++ suivi_indicateurs_soliguide_36.py
@@ -178,7 +178,7 @@ st.markdown('### Figure 5: Nombre de fiches suivies (en ligne et en brouillon) p
 def main():
   html_temp = """<div class='tableauPlaceholder' id='viz1645457092765' style='position: relative'><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Nombredefichesparcommunes&#47;Tableaudebord1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1645457092765');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
   components.html(html_temp, height = 1000)
-  if __name__ == "__main__":
-    main()
+if __name__ == "__main__":
+  main()
 
 
